509c8acd2a3ac3bf224d14cd2a15d5fd1d3b1a55
erose357 <erose357@gmail.com>
2017-12-13 11:09:32 -0700

    Adds get all foods query and refactors into model/controller files

--- server.js
+++ server.js
@@ -6,19 +6,13 @@ const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
 const Foods = require('./lib/models/foods')
 const FoodsController = require('./lib/controllers/foods')
+const MealsController = require('./lib/controllers/meals')
 
 app.set('port', process.env.PORT || 3000)
 app.use(bodyParser.json())
 
 app.get('/api/v1/foods', FoodsController.getAllFoods)
-
-app.get('/api/v1/meals', (request, response) => {
-  database.raw('select m.*, array_agg(f_row) as foods from meals m inner join food_meals fm on fm.meal_id = m.id inner join (select f.* from foods f inner join food_meals fm on fm.food_id = f.id inner join meals m on m.id = fm.meal_id order by f.id) f_row on f_row.id = fm.food_id group by m.id')
-  .then((data) => {
-    if (!data) { return response.sendStatus(404) }
-    response.json(data.rows)
-  })
-})
+app.get('/api/v1/meals', MealsController.getAllMeals)
 
 if (!module.parent) {
   app.listen(app.get('port'), () => {

4933c50bf11f267719e27f5e56029885a05f1a63
erose357 <erose357@gmail.com>
2017-12-13 16:14:07 -0700

    Adds refactoring for get meal endpoint functions

--- server.js
+++ server.js
@@ -14,26 +14,8 @@ app.use(bodyParser.json())
 app.get('/api/v1/foods', FoodsController.getAllFoods)
 app.get('/api/v1/foods/:id', FoodsController.getSingleFood)
 
+app.get('/api/v1/meals/:meal_id/foods', MealsController.getSingleMeal)
 app.get('/api/v1/meals', MealsController.getAllMeals)
-app.get('/api/v1/meals/:meal_id/foods', (request, response) => {
-  let meal_id = request.params.meal_id
-  return database.raw(
-    `SELECT m.id, m.name, json_agg(foods_row) as foods
-    FROM meals m
-    INNER JOIN food_meals fm on fm.meal_id = m.id
-    INNER JOIN
-      (SELECT f.id, f.name, f.calories
-      FROM foods f
-      INNER JOIN food_meals fm  on fm.food_id = f.id
-      INNER JOIN meals m on m.id = fm.meal_id
-      ORDER BY f.id)
-    foods_row on foods_row.id = fm.food_id where m.id = ${meal_id}
-    GROUP BY m.id`)
-  .then((data) => {
-    if (!data) { return response.sendStatus(404) }
-    response.json(data.rows)
-  })
-})
 
 if (!module.parent) {
   app.listen(app.get('port'), () => {

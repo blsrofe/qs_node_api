bb0f15132d9762d7995504537b0ed00e589a5f5b
erose357 <erose357@gmail.com>
2017-12-12 20:46:25 -0700

    Able to get meals with food items, formatting is not perfect, needs refactor

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.use(bodyParser.json())
 
 app.get('/api/v1/foods', FoodsController.getAllFoods)
 
+app.get('/api/v1/meals', (request, response) => {
+  database.raw('select m.*, array_agg(f_row) as foods from meals m inner join food_meals fm on fm.meal_id = m.id inner join (select f.* from foods f inner join food_meals fm on fm.food_id = f.id inner join meals m on m.id = fm.meal_id order by f.id) f_row on f_row.id = fm.food_id group by m.id')
+  .then((data) => {
+    if (!data) { return response.sendStatus(404) }
+    response.json(data.rows)
+  })
+})
+
 if (!module.parent) {
   app.listen(app.get('port'), () => {
     console.log(`App running on ${app.get('port')}.`)

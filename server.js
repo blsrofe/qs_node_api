50f6e237946b394b7daad8026d6db382deae636d
erose357 <erose357@gmail.com>
2017-12-13 18:26:08 -0700

    Adds POST route for meals, needs refactor

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const database = require('knex')(configuration)
 const Foods = require('./lib/models/foods')
 const FoodsController = require('./lib/controllers/foods')
 const MealsController = require('./lib/controllers/meals')
+const Meals = require('./lib/models/meals')
 
 app.set('port', process.env.PORT || 3000)
 app.use(bodyParser.json())
@@ -26,16 +27,29 @@ app.put('/api/v1/foods/:id', FoodsController.editFood)
 app.get('/api/v1/meals/:meal_id/foods', MealsController.getSingleMeal)
 app.get('/api/v1/meals', MealsController.getAllMeals)
 app.post('/api/v1/meals/:meal_id/foods/:id', (request, response) => {
+  let food, meal;
   let id = request.params.id
   let meal_id = request.params.meal_id
-  database.raw(`
-    INSERT INTO food_meals (food_id, meal_id, created_at)
-    VALUES (?, ?, ?)
-    RETURNING food_id, meal_id`,
-    [id, meal_id, new Date]
-  )
+  Foods.fetchSingle(id)
   .then((data) => {
-    return data.rows
+    if(!data) { return response.status(404).send({ error: "Food not found" }) }
+    food = data
+  })
+  .then(() => {
+    return database.raw(`SELECT m.id, m.name FROM meals m where m.id = ?`, [meal_id])
+  })
+  .then((data) => {
+    if(!data) { return response.status(404).send({ error: "Meal not found" }) }
+    meal = data.rows
+  })
+  .then(() => {
+  return database.raw(`
+    INSERT INTO food_meals (food_id, meal_id, created_at)
+    VALUES (?, ?, ?)`,
+    [id, meal_id, new Date])
+  })
+  .then(() => {
+    return response.status(201).send({ success: `${food["name"]} has been added to ${meal[0]["name"]}`})
   })
 })
 
